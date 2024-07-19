63e4f50cb6df8897377a7ef048c66795133effc8
mpalani19 <palanim1911@gmail.com>
2024-07-20 00:30:02 +0530

    user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const {users} = require("./data/users.json")
 
 const app = express();
 
@@ -7,10 +8,136 @@ const PORT = 8081;
 
 app.use(express.json());
 
-// http://localhost:8081/
 app.get("/", (req, res)=> {
     res.status(200).json({
         message: "Server is up and running"
+    })
+})
+
+/**
+*route: /users
+*method:get
+*descriptions: get all users
+*access:Public
+*parameter:None
+*/
+
+app.get("/users",(req,res)=>{
+    res.status(200).json({
+        success: true,
+        data:users
+    })
+})
+
+/**
+*route: /users/:id
+*method:get
+*descriptions: get all users by their id
+*access:Public
+*parameter:id
+*/
+
+app.get("/users/:id",(req,res)=>{
+    const {id} = req.params;
+    const user=users.find((each)=> each.id === id);
+    if (!user){
+        res.status(404).json({
+            success:false,
+            message:"User Not Found For The Given Id"
+        })
+    }
+    return res.status(200).json({
+        success:true,
+        data:user
+    })
+})
+
+/**
+*route: /users
+*method:post
+*descriptions: create new user
+*access:Public
+*parameter:None
+*/
+
+app.post("/users",(req,res)=>{
+    const {id,name,surname,email,subscriptionType, subscriptionDate} = req.body;
+    const user=users.find((each)=>each.id===id);
+    if (user){
+        return res.status(404).json({
+            success:false,
+            message:"User with the given Id exist"
+        })
+    }
+    users.push(
+        {id,name,surname,email,subscriptionType, subscriptionDate
+        })
+        return res.status(201).json({
+            success: true,
+            data: users
+        })
+})
+
+/**
+*route: /users/:id
+*method:put
+*descriptions: update user by their ID
+*access:Public
+*parameter: ID
+*/
+
+app.put('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    const {data} = req.body;
+
+    const user = users.find((each)=> each.id === id);
+    if(!user){
+        return res.status(404).json({
+            success: false,
+            message: "User Not Found For The Given Id :-("
+        })
+    }
+
+    const updateUser = users.map((each)=>{
+        if(each.id===id){
+            return {
+                ...each,
+                ...data
+            }
+        }
+        return each;
+    })
+    return res.status(200).json({
+        success: true,
+        data: updateUser
+    })
+
+})
+
+/**
+*route: /users/:id
+*method:Delete
+*descriptions: Delete a user by their id
+*access:Public
+*parameter:id
+*/
+
+app.delete("/users/:id",(req,res)=>{
+    const {id} = req.params;
+
+    const user=users.find((each)=> each.id===id);
+    if(!user){
+        return res.status(404).json({
+            success:false,
+            message:"User not found for the given id"
+        })
+    }
+    const index = users.indexOf(user);
+    users.splice(index,1);
+
+    return res.status(200).json({
+        success:true,
+        data: users
     })
 })
 
